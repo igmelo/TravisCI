045946dc46d18f54d8a00b1a91377756ec821f54
igmelo <36697517+igmelo@users.noreply.github.com>
2018-10-25 08:39:35 -0300

    Add files via upload

---
+++ src/main/java/br/inatelec206/integracaocontinua/IntegracaoContinua.java
@@ -0,0 +1,9 @@
+package br.inatelec206.integracaocontinua;
+
+public class IntegracaoContinua {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    }
+}
